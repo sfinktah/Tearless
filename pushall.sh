526874687da1fc5add7ae925d579766f7a653352
sfinktah <sfinktah@bitbucket.spamtrak.org>
2016-09-09 13:16:07 +1000

    added windowskit81 for old dx builds

--- pushall.sh
+++ pushall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PWD=$PWD
+PWD $PWD
 echo PWD "$PWD"
 for fn in * .; do
     echo "Directory: $fn"
@@ -7,10 +7,26 @@ for fn in * .; do
     (
         cd "$PWD"
         cd "$fn"
-        git status
-        git add -u
-        git commit -m "$@"
-        git push origin master
+        git config core.autocrlf true
+        git-ad | grep 'HEAD detached' && {
+            echo fucking head detached "$PWD/$fn"
+            git branch | grep '^..temp$' && {
+                git branch -d temp || {
+                        echo "ABORT! temp branch is currently live, fix it"
+                        exit
+                }
+            }
+            git fetch &&
+            git checkout -b temp && 
+            git checkout -B master temp && 
+            git merge origin master -m 'pushall' &&
+            git push && 
+            git branch -d temp
+        }
+        # git status
+        # git add u
+        # git commit -m "$@"
+        # git push origin master
     )
     echo "$PWD"
 done
